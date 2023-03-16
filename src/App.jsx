c620bb2f33e10ebdcf8d92335e30644089a05ec4
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-16 14:49:19 -0400

    added fetching from API to getMovie

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import './App.css';
 import Form from './components/Form';
 import MovieDisplay from './components/MovieDisplay';
 
+const API_KEY = 'd222b784'
+const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
+
+
 function App()
 {
-  const getMovie = (searchTerm) =>
+  const getMovie = async (searchTerm) =>
   {
     console.log(`fetching data about ${searchTerm}...`)
+    const response = await fetch(`${BASE_URL}t=${searchTerm}`)
+    console.log(response)
+    let resBody = await response.json()
+    console.log(resBody)
   }
 
   return (
