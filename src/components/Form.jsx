ab88bc8b7b62dcc24f9ac13ecd0d18747ff855cb
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-16 14:36:38 -0400

    refactored form to be controlled

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,40 @@
-import { useRef } from "react"
+import { useState, useRef } from "react"
+
+
 
 export default function Form(props)
 {
     const { movieSearch } = props
 
+    const DEFAULT_FORM_STATE = {
+        searchTerm: '',
+        username: ''
+    }
+
+    // useful to store form data as an object so that we don't need state for each input/piece of data being tracked
+    const [formData, setFormData] = useState(DEFAULT_FORM_STATE)
+
     const searchRef = useRef(null)
+    const usernameRef = useRef(null)
+
+    const handleChange = (e) =>
+    {
+        setFormData(prevData =>
+        {
+            // set formData to object with all previous values, but overwrite value of key where key is equal to the name assigned to the given input
+            return { ...prevData, [e.target.name]: e.target.value }
+        })
+    }
 
     const handleSubmit = (e) =>
     {
         e.preventDefault()
-        console.log(`submitting search for ${searchRef.current.value}...`)
-        movieSearch(searchRef.current.value)
-        searchRef.current.value = ''
+        for (let [key, value] of Object.entries(formData))
+        {
+            console.log(`key: ${key}  value: ${value}`)
+        }
+        movieSearch(formData.searchTerm)
+        setFormData(DEFAULT_FORM_STATE)
     }
 
     return (
@@ -20,7 +43,17 @@ export default function Form(props)
                 <input
                     ref={searchRef}
                     type="text"
-                    placeholder="Movie title goes here" />
+                    placeholder="Movie title goes here"
+                    value={formData.searchTerm}
+                    name="searchTerm"
+                    onChange={handleChange} />
+                <input
+                    ref={usernameRef}
+                    type="text"
+                    placeholder="Username goes here"
+                    value={formData.username}
+                    name="username"
+                    onChange={handleChange} />
                 <input type="submit" value="Submit" />
             </form>
         </div>
