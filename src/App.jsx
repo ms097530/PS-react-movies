7f0861966746a424b5ee2f6b62fdeac4504ec866
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-16 15:58:47 -0400

    standardized first level of movie data keys, added content to MovieDisplay

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
 
 function App()
 {
+  const [movie, setMovie] = useState(null)
   const getMovie = async (searchTerm) =>
   {
     console.log(`fetching data about ${searchTerm}...`)
@@ -16,12 +17,26 @@ function App()
     console.log(response)
     let resBody = await response.json()
     console.log(resBody)
+
+    // standardize key values so the first letters are not capitalized
+    // TODO: make recursive
+    let movieData = {}
+    for (let [key, value] of Object.entries(resBody))
+    {
+      let firstLetterLower = key.charAt(0).toLowerCase()
+      let newKey = firstLetterLower + key.slice(1)
+      movieData[newKey] = value
+    }
+    // if match is found, save movie data, else set to null
+    setMovie(movieData.response === 'True' ? movieData : null)
   }
 
   return (
     <div className="App">
       <Form movieSearch={getMovie} />
-      <MovieDisplay />
+      {
+        movie && <MovieDisplay movie={movie} />
+      }
     </div>
   );
 }
--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,6 +1,47 @@
-export default function MovieDisplay()
+import { useEffect, useState } from "react"
+
+export default function MovieDisplay(props)
 {
+    const { movie } = props
+
+    const [showMore, setShowMore] = useState(false)
+
+    useEffect(() =>
+    {
+        setShowMore(false)
+    }, [movie])
+
     return (
-        <div>MovieDisplay</div>
+        <div className="MovieDisplay">
+            <h1>{movie.title}</h1>
+            <div className="MovieDisplay__img-container">
+                <img src={movie.poster} alt={movie.title} />
+            </div>
+            <h2>Released: {movie.released}</h2>
+            <h2>Genre(s): {movie.genre}</h2>
+            <p>{movie.plot}</p>
+            {
+                !showMore && <p className="MovieDisplay__more" onClick={() => setShowMore(true)}>more...</p>
+            }
+            {
+                showMore && (
+                    <>
+                        <p>Directed by: {movie.director}</p>
+                        <p>Written by: {movie.writer}</p>
+                        <p>Actors: {movie.actors}</p>
+                        <p>Duration: {movie.runtime}</p>
+
+                        {
+                            movie.ratings.map(rating => (
+                                <>
+                                    <h3>Rated by: {rating.Source}</h3>
+                                    <p>Score: {rating.Value}</p>
+                                </>
+                            ))
+                        }
+                    </>
+                )
+            }
+        </div>
     )
 }
