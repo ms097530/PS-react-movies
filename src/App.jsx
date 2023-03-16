e108b5719bad5e41255251c6e73b2d3468fb912d
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-16 13:31:27 -0400

    created skeleton for uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import MovieDisplay from './components/MovieDisplay';
 
 function App()
 {
+  // const [searchText, setSearchText] = useState('')
+
+  const getMovie = (searchTerm) =>
+  {
+    console.log('fetching data...')
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form /* text={searchText} setText={setSearchText} */
+        movieSearch={getMovie} />
       <MovieDisplay />
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,34 @@
-export default function Form()
+import { useRef } from "react"
+
+export default function Form(props)
 {
+    // const { text, setText } = props
+    const { movieSearch } = props
+
+    const searchRef = useRef(null)
+
+    // const handleKeyDown = (e) =>
+    // {
+    //     console.log(e)
+    //     setText(prevText => prevText + e.key)
+    // }
+
+    const handleSubmit = (e) =>
+    {
+        e.preventDefault()
+        console.log(`submitting search for ${searchRef.current.value}...`)
+        movieSearch(searchRef.current.value)
+    }
+
     return (
         <div>
-            <form action="">
-                <input type="text" placeholder="Movie title goes here" />
+            <form action="" onSubmit={handleSubmit}>
+                <input
+                    ref={searchRef}
+                    type="text"
+                    placeholder="Movie title goes here"
+                    /* onKeyDown={handleKeyDown} 
+                    value={text} */ />
                 <input type="submit" value="Submit" />
             </form>
         </div>
