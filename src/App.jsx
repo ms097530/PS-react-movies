a5f6b597e48d3b55c6ed89fb8644d9b71970b013
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-17 10:10:29 -0400

    moved OMDB api logic and refactored usage in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,59 @@
 import { useState, useEffect } from 'react';
+import { getMovie } from './services/omdbapi';
 import './App.css';
 import Form from './components/Form';
 import MovieDisplay from './components/MovieDisplay';
 
-const API_KEY = 'd222b784'
-const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
+
 
 
 function App()
 {
   const [movie, setMovie] = useState(null)
-  const getMovie = async (searchTerm) =>
-  {
-    console.log(`fetching data about ${searchTerm}...`)
-    const response = await fetch(`${BASE_URL}t=${searchTerm}`)
-    console.log(response)
-    let resBody = await response.json()
-    console.log(resBody)
+  // const getMovie = async (searchTerm) =>
+  // {
+  //   try
+  //   {
+  //     console.log(`fetching data about ${searchTerm}...`)
+  //     const response = await fetch(`${BASE_URL}t=${searchTerm}`)
+  //     console.log(response)
+  //     let resBody = await response.json()
+  //     console.log(resBody)
 
-    // standardize key values so the first letters are not capitalized
-    // TODO: make recursive
-    let movieData = {}
-    for (let [key, value] of Object.entries(resBody))
-    {
-      let firstLetterLower = key.charAt(0).toLowerCase()
-      let newKey = firstLetterLower + key.slice(1)
-      movieData[newKey] = value
-    }
-    // if match is found, save movie data, else set to null
-    setMovie(movieData.response === 'True' ? movieData : null)
-  }
+  //     // standardize key values so the first letters are not capitalized
+  //     // TODO: make recursive
+  //     let movieData = {}
+  //     for (let [key, value] of Object.entries(resBody))
+  //     {
+  //       let firstLetterLower = key.charAt(0).toLowerCase()
+  //       let newKey = firstLetterLower + key.slice(1)
+  //       movieData[newKey] = value
+  //     }
+  //     // if match is found, save movie data, else set to null
+  //     setMovie(movieData.response === 'True' ? movieData : null)
+  //   }
+  //   catch (e)
+  //   {
+  //     console.error(e)
+  //   }
+  // }
 
   // initialize with movie
   useEffect(() =>
   {
-    setTimeout(() => getMovie('Clueless'), 5000)
-
+    async function initializeMovie()
+    {
+      let movie = await getMovie('Clueless')
+      // console.log(movie)
+      // simulate loading
+      setTimeout(() => setMovie(movie), 5000)
+    }
+    initializeMovie()
   }, [])
 
   return (
     <div className="App">
-      <Form movieSearch={getMovie} />
+      <Form movieSearch={async (movie) => { let newMovie = await getMovie(movie); setMovie(newMovie); }} />
       <MovieDisplay movie={movie} />
     </div>
   );
---
+++ src/services/omdbapi.js
@@ -0,0 +1,32 @@
+const API_KEY = 'd222b784'
+const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
+
+export const getMovie = async (searchTerm) =>
+{
+    try
+    {
+        // console.log(`fetching data about ${searchTerm}...`)
+        const response = await fetch(`${BASE_URL}t=${searchTerm}`)
+        // console.log(response)
+        let resBody = await response.json()
+        // console.log(resBody)
+
+        // standardize key values so the first letters are not capitalized
+        // TODO: make recursive
+        let movieData = {}
+        for (let [key, value] of Object.entries(resBody))
+        {
+            let firstLetterLower = key.charAt(0).toLowerCase()
+            let newKey = firstLetterLower + key.slice(1)
+            movieData[newKey] = value
+        }
+        // if match is found, save movie data, else set to null
+        // setMovie(movieData.response === 'True' ? movieData : null)
+        // console.log('movieData:', movieData)
+        return movieData
+    }
+    catch (e)
+    {
+        console.error(e)
+    }
+}
